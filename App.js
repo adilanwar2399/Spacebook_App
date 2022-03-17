9f0e5ba51aa42603e27792fe92a89c815842cdb9
adilanwar2399 <56471898+adilanwar2399@users.noreply.github.com>
2022-03-17 12:02:28 +0000

    This file was updated with Nested  Navigation
    Stack and Tab Navigation were coincided and therefore used together allowing smoother functionality of the App.

--- App.js
+++ App.js
@@ -1,33 +1,69 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './components/home';
 import LoginScreen from './components/login';
 import SignupScreen from './components/signup';
 import LogoutScreen from './components/logout';
-import FeedScreen from './components/feed';
 import ProfileScreen from './components/Profile';
 import FriendScreen from './components/Friends';
-import ProfilePictureUploadScreen from './components/ProfilePicUpload';
+import FriendRequestsScreen from './components/FriendRequests';
+import PhotoCaptureScreen from './components/PhotoCapture';
+import PostsScreen from './components/Posts';
+import UserScreen from './components/User';
+import UserUpdateScreen from './components/userInfoUpdate';
+import SearchScreen from './components/Search';
 
+
+export const serverBase = 'http://localhost:3333/api/1.0.0/'
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
+
+
+function TabNavigation() {
+  return(
+  <Tab.Navigator>
+    <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Screen name="Friend Requests" component={FriendRequestsScreen} />
+    <Tab.Screen name="Search" component={SearchScreen} />
+    <Tab.Screen name="Logout" component={LogoutScreen} />
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="User Update" component={UserUpdateScreen} />
+  </Tab.Navigator> 
+  );
+}
 class App extends Component {
 
   render(){
     return (
       <NavigationContainer>
-        <Tab.Navigator>
+        
+        {/* <Tab.Navigator>
           <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+          <Tab.Screen name="Friend Requests" component={FriendRequestsScreen} />
+          <Tab.Screen name="Logout" component={LogoutScreen} />
+          <Tab.Screen name="Posts" component={PostsScreen} />
+          <Tab.Screen name="User" component={UserScreen} />
+          <Tab.Screen name="User Update" component={UserUpdateScreen} />
+          <Tab.Screen name="Photo Capture" component={PhotoCaptureScreen} /> 
           <Tab.Screen name="Signup" component={SignupScreen} />
           <Tab.Screen name="Login" component={LoginScreen} />
-          <Tab.Screen name="Logout" component={LogoutScreen} />
-          <Tab.Screen name="Feed" component={FeedScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-          <Tab.Screen name="Friends" component={FriendScreen} />
-          <Tab.Screen name="Profile Picture Upload" component={ProfilePictureUploadScreen} />
-        </Tab.Navigator>
+          <Tab.Screen name="Friends" component={FriendScreen} />  
+        </Tab.Navigator>  */}
+         <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Posts" component={PostsScreen} />
+          <Stack.Screen name="ProfileScreen" component={TabNavigation} options={{headerShown: false, cardStyle: {flex: 1}}} />
+          <Stack.Screen name="User" component={UserScreen} />
+          <Stack.Screen name="User Update" component={UserUpdateScreen} />
+          <Stack.Screen name="Photo Capture" component={PhotoCaptureScreen} /> 
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="Friends" component={FriendScreen} />
+          </Stack.Navigator>
       </NavigationContainer>
     );
   }
