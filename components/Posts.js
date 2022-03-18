aa4ec30ea8c8c7bb13c06e3d7f3fa3e75d91f305
adilanwar2399 <56471898+adilanwar2399@users.noreply.github.com>
2022-03-18 05:06:32 +0000

    Update Posts Screen

--- components/Posts.js
+++ components/Posts.js
@@ -9,19 +9,22 @@ class PostsScreen extends Component{
 
         this.state = {
             userID: this.props.route.params.userInfo,
-            postID: this.props.route.params.item,
             userName: this.props.route.params.userName,
             // userID: 1,
             // postID: 2,
             dataPosting: [],
             textPosting: "",
             postEditing: false,
+            postID: this.props.route.params.item,
+            //To see whether the user is verified or not.
             verifiedUser: false,
             updating: false, 
             titleForLikes: "Like", 
             //likesQuantity: 0, 
             textForChecks: "",
-            myProfile: false
+            myProfile: false,
+           // numLikes: ""
+            
         }
     }
 
@@ -71,6 +74,7 @@ class PostsScreen extends Component{
         })
         .then((responseJson) => {
             if(id == responseJson.author.user_id){
+                console.log(responseJson)
                 this.setState({
                     dataPosting: responseJson,
                     verifiedUser: true
@@ -80,6 +84,7 @@ class PostsScreen extends Component{
                     dataPosting: responseJson,
                     verifiedUser: false
                 })
+
             }
             
         })
@@ -135,8 +140,8 @@ class PostsScreen extends Component{
     likedChecks = async () =>{
         //Here an instant check is performed to determine whether the post(s) are liked or not
         //What will happen is that the unliked posts will be liked.
-        const id = await AsyncStorage.getItem('@user_id')
         const token = await AsyncStorage.getItem('@session_token')
+        const id = await AsyncStorage.getItem('@user_id')
         if(id != this.state.userID){
             return fetch(serverBase+"user/"+this.state.userID+"/post/"+this.state.postID+"/like", {
                 'method': 'POST',
@@ -145,20 +150,6 @@ class PostsScreen extends Component{
                 }
             })
             .then((response) => {
-                // if(response.status === 200){
-                //     return fetch(serverBase+"user/"+this.state.userID+"/post/"+this.state.postID+"/like", {
-                //         'method': 'DELETE',
-                //         'headers':{
-                //             'X-Authorization': token
-                //         }
-                //     })
-                
-                // }else if(response.status === 403 || response.status === 400){
-                //     this.setState({
-                //         titleForLikes: "unlike"
-                //     })
-                // }
-
                 if(response.status === 403 || response.status === 400){
                     this.setState({
                         titleForLikes: "unlike"
@@ -179,9 +170,65 @@ class PostsScreen extends Component{
         }
     }
 
+    likePosts = async (user_id, post_id) => {
+        const value = await AsyncStorage.getItem('@session_token');
+        const valueId = await AsyncStorage.getItem('@user_id');
+            return fetch(serverBase + "user/" +valueId+ "/post/" +post_id+ "/like", {
+            method: 'Post',
+            headers: {
+                'X-Authorization': value 
+            }
+        })
+        .then((response) =>{
+            if(response.status === 200){
+                console.log("200: Good Respose")
+            }else if(response.status === 401){
+                this.props.navigation.navigate("Login")
+            }else if(response.status === 403){
+                console.log("403 Forbidden Request; restricted access ")
+            }else{
+                throw "Something went wrong"
+            }
+        })
+        .then((responseJson) => {
+            console.log('Liked: ', responseJson)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    unLikePosts = async (user_id, post_id) => {
+        const value = await AsyncStorage.getItem('@session_token');
+        const idValue = await AsyncStorage.getItem('@user_id');
+        return fetch(serverBase + "user/" +idValue+ "/post/" +post_id+ "/like", {
+          method: 'Delete',
+          headers: {
+            'X-Authorization': value 
+        }
+      })
+        .then((response) =>{
+            if(response.status === 200){
+                console.log("200: Good Respose")
+            }else if(response.status === 401){
+                this.props.navigation.navigate("Login")
+            }else if(response.status === 403){
+                console.log("403 Forbidden Request; restricted access ")
+            }else{
+                throw "Something went wrong"
+            }
+        })
+        .then((responseJson) => {
+            console.log('Un-Liked: ', responseJson)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+      }
 
     postLike = async () =>{
         let requestForLikes
+        
         if(this.state.titleForLikes =="Like"){
             requestForLikes = "POST"
             this.setState({
@@ -254,105 +301,80 @@ class PostsScreen extends Component{
                     <View>
                         <ScrollView>
                             <View>
-                                <Text style={styles.stylesForLabels}>Notification: Someone has Posted.</Text>
-                                <Text style={styles.stylesForLabels}>Posted to {this.state.userName}'s Account Feed.</Text>
-                                <Text style={styles.stylesForLabels}>The Post was made by {this.state.authorName}</Text>
+                                <Text style={styles.stylesForLabels}>
+                                    Notification: Post has been made to..
+                                </Text>
+                                <Text style={styles.stylesForLabels}>
+                                    {this.state.userName}'s Account Feed.
+                                </Text>
+                                <Text style={styles.stylesForLabels}>
+                                    The Post was made by {this.state.authorName}
+                                </Text>
                             </View>
 
-                            <TextInput
-                                style={styles.styleOfPosts}
-                                multiline
-                                editable={this.state.postEditing}
-                                numberOfLines={4}
+                            <TextInput style={styles.styleOfPosts}
+                                multiline // ensures that the user text imput accepts multiple lines
+                                editable={this.state.postEditing} // checks whether the text is editable or not
+                                numberOfLines={10} // the maximum number of lines the text input can store
                                 onChangeText={(value) => this.setState({textPosting: value})}
                                 defaultValue={this.state.dataPosting.text}
                             />
-                            <Text>{this.state.textForChecks}</Text>
-                            <Text style={styles.label}>Likes: {this.state.dataPosting.numLikes}</Text>
-                            <Button
+                            <Text>
+                                {this.state.textForChecks}
+                            </Text>
+                            <Text 
+                                style={styles.stylesForLabels}>Likes: {this.state.dataPosting.numLikes}
+                            </Text>
+                            <Button style = {styles.buttonStyle}
                                 title="Save"
                                 onPress={() => this.postUpdate()}
                             />
-                            <Button
+                            <Button style = {styles.buttonStyle}
                                 title="Back"
                                 onPress={(value) => this.setState({postEditing: false}, () => this.getPostData())}Yea
                                 value={this.state.postEditing}
                             />
                         </ScrollView>
                     </View>
-
-                    // <ScrollView>
-                    //     <TextInput
-                    //         style={styles.styleOfPosts}
-                    //         multiline
-                    //         editable={this.state.postEditing}
-                    //         numberOfLines={4}
-                    //         onChangeText={(value) => this.setState({textPosting: value})}
-                    //         defaultValue={this.state.dataPosting.text}
-                    //     />
-                    //     <Button
-                    //         title="Update"
-                    //         onPress={(value) => this.setState({postEditing: value})}
-                    //         value={this.state.postEditing}
-                            
-                    //     />
-                    //     <Button
-                    //         title="Delete"
-                    //         onPress={() => this.postDelete()}
-                    //     />
-                    // </ScrollView>
                 )
             }else{
                 return(
                     <View>
                         <ScrollView>
                             <View>
-                                <Text style={styles.stylesForLabels}>Notification: Someone has Posted.</Text>
-                                <Text style={styles.stylesForLabels}>Posted to {this.state.userName}'s Account Feed.</Text>
-                                <Text style={styles.stylesForLabels}>The Post was made by {this.state.authorName}</Text>
+                                <Text style={styles.stylesForLabels}>
+                                    Notification: Someone has Posted.
+                                </Text>
+                                <Text style={styles.stylesForLabels}>
+                                    Posted to {this.state.userName}'s Account Feed.
+                                </Text>
+                                <Text style={styles.stylesForLabels}>
+                                    The Post was made by {this.state.authorName}
+                                </Text>
                             </View>
 
-                            <TextInput
-                                style={styles.styleOfPosts}
-                                multiline
-                                editable={this.state.postEditing}
-                                numberOfLines={4}
+                            <TextInput style={styles.styleOfPosts}
+                                multiline // ensures that the user text imput accepts multiple lines
+                                editable={this.state.postEditing} // checks whether the text is editable or not
+                                numberOfLines={10} // the maximum number of lines the text input can store
                                 onChangeText={(value) => this.setState({textPosting: value})}
                                 defaultValue={this.state.dataPosting.text}
                             />
-                            <Text style={styles.label}>Likes: {this.state.dataPosting.numLikes}</Text>
-                            <Button
+                            <Text style={styles.stylesForLabels}>
+                                Likes: {this.state.dataPosting.numLikes}
+                            </Text>
+                            <Button style = {styles.buttonStyle}
                                 title="Update"
                                 onPress={(value) => this.setState({postEditing: value})}
                                 value={this.state.postEditing}
                                 
                             />
-                            <Button
+                            <Button style = {styles.buttonStyle}
                                 title="Delete"
                                 onPress={() => this.postDelete()}
                             />
                         </ScrollView>
                     </View>
-                    // <ScrollView>
-                    //     <TextInput
-                    //         style={styles.styleOfPosts}
-                    //         multiline
-                    //         editable={this.state.postEditing}
-                    //         numberOfLines={4}
-                    //         onChangeText={(value) => this.setState({textPosting: value})}
-                    //         defaultValue={this.state.dataPosting.text}
-                    //     />
-                    //     <Text>{this.state.textForChecks}</Text>
-                    //     <Button
-                    //         title="Save"
-                    //         onPress={() => this.postUpdate()}
-                    //     />
-                    //     <Button
-                    //         title="Back"
-                    //         onPress={(value) => this.setState({postEditing: false}, () => this.getPostData())}Yea
-                    //         value={this.state.postEditing}
-                    //     />
-                    // </ScrollView>
                 )
             }
         }else{
@@ -361,68 +383,61 @@ class PostsScreen extends Component{
                     <View>
                         <ScrollView>
                             <View>
-                                <Text style={styles.stylesForLabels}>Notification: Someone has Posted.</Text>
-                                <Text style={styles.stylesForLabels}>Posted to {this.state.userName}'s Account Feed.</Text>
-                                <Text style={styles.stylesForLabels}>The Post was made by {this.state.authorName}</Text>
+                                <Text style={styles.stylesForLabels}>
+                                    Notification: Someone has Posted.
+                                </Text>
+                                <Text style={styles.stylesForLabels}>
+                                    Posted to {this.state.userName}'s Account Feed.
+                                </Text>
+                                <Text style={styles.stylesForLabels}>
+                                    The Post was made by {this.state.authorName}
+                                </Text>
                             </View>
 
-                            <TextInput
-                                style={styles.styleOfPosts}
-                                multiline
-                                editable={false}
-                                numberOfLines={4}
+                            <TextInput style={styles.styleOfPosts}
+                                multiline // ensures that the user text imput accepts multiple lines
+                                editable={this.state.postEditing} // checks whether the text is editable or not
+                                numberOfLines={10} // the maximum number of lines the text input can store
                                 defaultValue={this.state.dataPosting.text}
                             />
-                            <Text style={styles.label}> Number of Likes: {this.state.postData.numLikes}</Text>
+                            <Text style={styles.stylesForLabels}> 
+                                Number of Likes: {this.state.dataPosting.numLikes}
+                            </Text>
                         </ScrollView>
                     </View>
-                    // <ScrollView>
-                    //     <TextInput
-                    //         style={styles.styleOfPosts}
-                    //         multiline
-                    //         editable={false}
-                    //         numberOfLines={4}
-                    //         defaultValue={this.state.dataPosting.text}
-                    //     />
-                    //     <Button
-                    //         title={this.state.titleForLikes}
-                    //         onPress={() => this.postLike()}
-                    //     />
-                    // </ScrollView>
                 )
             }else{
                 return(
                     <View>
                         <ScrollView>
                             <View>
-                                <Text style={styles.stylesForLabels}>Notification: Someone has Posted.</Text>
-                                <Text style={styles.stylesForLabels}>Posted to {this.state.userName}'s Account Feed.</Text>
-                                <Text style={styles.stylesForLabels}>The Post was made by {this.state.authorName}</Text>
+                                <Text style={styles.stylesForLabels}>
+                                    Notification: Someone has Posted.
+                                </Text>
+                                <Text style={styles.stylesForLabels}>
+                                    Posted to {this.state.userName}'s Account Feed.
+                                </Text>
+                                <Text style={styles.stylesForLabels}>
+                                    The Post was made by {this.state.authorName}
+                                </Text>
                             </View>
 
-                            <TextInput
-                                style={styles.styleOfPosts}
-                                multiline
-                                editable={false}
-                                numberOfLines={4}
+                            <TextInput style={styles.styleOfPosts}
+                                multiline // ensures that the user text imput accepts multiple lines
+                                editable={this.state.postEditing} // checks whether the text is editable or not
+                                numberOfLines={10} // the maximum number of lines the text input can store
                                 defaultValue={this.state.dataPosting.text}
                             />
                             <Button
                                 title={this.state.titleForLikes}
                                 onPress={() => this.postLike()}
                             />
-                            <Text style={styles.label}> Number of Likes: {this.state.postData.numLikes}</Text>
+                            <Text style={styles.stylesForLabels}> 
+                                Number of Likes: {this.state.dataPosting.numLikes}
+                            </Text>
                         </ScrollView>
                     </View>
-                    // <ScrollView>
-                    //     <TextInput
-                    //         style={styles.styleOfPosts}
-                    //         multiline
-                    //         editable={false}
-                    //         numberOfLines={4}
-                    //         defaultValue={this.state.dataPosting.text}
-                    //     />
-                    // </ScrollView>
+         
                 )
             }
         }
@@ -430,15 +445,19 @@ class PostsScreen extends Component{
 }
 
 const styles = StyleSheet.create({
-    stylesForLabels: {
-        fontSize:20,
-        color:'slateblue'
-    },
     styleOfPosts: {
         borderWidth:2,
         borderColor: 'lightblue',
         borderRadius:5,
-      }
+    },
+    stylesForLabels: {
+        fontSize: 16,
+        color:'slateblue'
+    },
+    buttonStyle: {
+        color: 'steelblue'
+
+    }
 })
 
 export default PostsScreen;
